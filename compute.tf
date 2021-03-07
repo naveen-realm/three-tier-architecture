a89c731893efa5364b9fe7f0bb43c400c57782d0
Naveen Doddaiah <43662840+naveen-realm@users.noreply.github.com>
2021-03-07 20:09:10 +0530

    parameterized the components

--- compute.tf
+++ compute.tf
@@ -1,29 +1,25 @@
 data "aws_ssm_parameter" "linux_latest_ami" {
-  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+  name = var.compute_image_name
 }
 
 resource "aws_instance" "jump_box" {
   ami           = data.aws_ssm_parameter.linux_latest_ami.value
-  instance_type = "t2.micro"
-  key_name      = "sample_app_key"
+  instance_type = var.compute_instance_type
+  key_name      = var.compute_key_name
 
   subnet_id              = aws_subnet.pub_sub.id
   vpc_security_group_ids = [aws_security_group.general_sg.id, aws_security_group.bastion_sg.id]
 
-  tags = {
-    Project = "sample-app"
-  }
+  tags = var.compute_instance_tag
 }
 
 resource "aws_instance" "app_instance" {
   ami           = data.aws_ssm_parameter.linux_latest_ami.value
-  instance_type = "t2.micro"
-  key_name      = "sdn_tutorial_key"
+  instance_type = var.compute_instance_type
+  key_name      = var.compute_key_name
 
   subnet_id              = aws_subnet.prv_sub.id
   vpc_security_group_ids = [aws_security_group.general_sg.id, aws_security_group.app_sg.id]
 
-  tags = {
-    Project = "sample-app"
-  }
+  tags = var.compute_instance_tag
 }

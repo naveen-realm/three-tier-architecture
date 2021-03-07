8deb2dd5ca5e2cce5c0ab27b9a60d1bd69b72aaa
Naveen Doddaiah <43662840+naveen-realm@users.noreply.github.com>
2021-03-07 22:50:15 +0530

    added database related variables

--- variables.tf
+++ variables.tf
@@ -18,3 +18,48 @@ variable "compute_image_name" {
   type        = string
   description = "The name of the parameter. This should be the instance image name present in aws ssm paramter store"
 }
+
+variable "database_identifier" {
+  type        = string
+  description = "The name of the RDS instance, if omitted, Terraform will assign a random, unique identifier"
+}
+
+variable "database_instance_class" {
+  type        = string
+  description = "The instance type of the RDS instance."
+}
+
+variable "database_allocated_storage" {
+  type        = number
+  description = "The allocated storage in gibibytes. If max_allocated_storage is configured, this argument represents the initial storage allocation and differences from the configuration will be ignored automatically when Storage Autoscaling occurs."
+}
+
+variable "database_engine" {
+  type        = string
+  description = "The database engine to use"
+}
+
+variable "database_name" {
+  type = string
+  description = "The name of the database to create when the DB instance is created. If this parameter is not specified, no database is created in the DB instance."
+ }
+
+variable "database_password" {
+  type        = string
+  description = "Password for the master DB user"
+}
+
+variable "database_username" {
+  type        = string
+  description = "Username for the master DB user"
+}
+
+variable "database_engine_version" {
+  type        = string
+  description = "The database engine version"
+}
+
+variable "database_skip_final_snapshot" {
+  type = bool
+  description = "Determines whether a final DB snapshot is created before the DB instance is deleted. If true is specified, no DBSnapshot is created"
+}
